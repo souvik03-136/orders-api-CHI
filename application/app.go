a401917e91e17515b6da9fecf621c38f531cd5c3
souvik03-136 <66234771+souvik03-136@users.noreply.github.com>
2024-04-24 02:07:47 +0530

    add

--- application/app.go
+++ application/app.go
@@ -16,9 +16,10 @@ type App struct {
 
 func New() *App {
 	app := &App{
-		router: loadRoutes(),
-		rdb:    redis.NewClient(&redis.Options{}),
+		rdb: redis.NewClient(&redis.Options{}),
 	}
+
+	app.loadRoutes()
 	return app
 }
 
--- application/routes.go
+++ application/routes.go
@@ -6,9 +6,10 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/souvik03-136/orders-api/handler"
+	"github.com/souvik03-136/orders-api/repository/order"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
@@ -17,11 +18,16 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 
 	})
-	router.Route("/orders", loadOrderRoutes)
-	return router
+	router.Route("/orders", a.loadOrderRoutes)
+	a.router = router
 }
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
 	router.Get("/{id}", orderHandler.GetByID)
